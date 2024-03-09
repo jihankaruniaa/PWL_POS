6b6b5551d52c439e7ef2e73dc977e1db029ea94e
Jihan Karunia Putri <jihankarunia09@gmail.com>
2024-03-09 22:21:21 +0700

    Praktikum 6 – Implementasi Eloquent ORM

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
