436619a51e704382e0d5824da22fe9be8c72a8ac
Jihan Karunia Putri <jihankarunia09@gmail.com>
2024-03-13 13:03:57 +0700

    Praktikum 2.1 – Retrieving Single Models

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -12,14 +12,12 @@
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
             </tr>
-            @foreach($data as $d)
             <tr>
                 <td>{{$d->user_id}}</td>
                 <td>{{$d->username}}</td>
                 <td>{{$d->name}}</td>
                 <td>{{$d->level_id}}</td>
             </tr>
-            @endforeach
         </table>
     </body>
 </html>
