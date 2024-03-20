2acce569d74fa1ee728d662f0541ee6f8a00b4e5
Jihan Karunia Putri <jihankarunia09@gmail.com>
2024-03-20 15:15:35 +0700

    Praktikum 2 – Integrasi dengan DataTables

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -3,27 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\DataTables\KategoriDataTable;
 
 class KategoriController extends Controller
 {
-    public function index()
+    public function index(KategoriDataTable $dataTable)
     {
-        // $data=[
-        //     'kategori_kode' => 'SNK',
-        //     'kategori_nama' => 'Snack / Makanan Ringan',
-        //     'created_at' => now()
-        // ];
-        // DB::table('m_kategori')->insert($data);
-        // return 'Insert Data Baru Berhasil!';
-
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
-        // return 'Update Data Berhasil. Jumlah Data yang Diupdate: '.$row. ' baris';
-
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
-        // return 'Delete Data Berhasil. Jumlah Data yang Dihapus: '.$row. ' baris';
-
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $data]);
+        return $dataTable->render('kategori.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']) ->n
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('user.ubah');
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('user.ubah_simpan');
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('user.hapus');
+
+Route::get('\kategori', [KategoriController::class, 'index']);
