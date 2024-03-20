6a8661a73d59294c09e9ba75084d7a530bb241d6
Jihan Karunia Putri <jihankarunia09@gmail.com>
2024-03-20 15:32:08 +0700

    Praktikum 3 – Membuat form kemudian menyimpan data dalam database

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('user.ubah'
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('user.ubah_simpan');
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('user.hapus');
 
-Route::get('\kategori', [KategoriController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/kategori/create', [KategoriController::class, 'create']);
+Route::post('/kategori', [KategoriController::class, 'store']);
