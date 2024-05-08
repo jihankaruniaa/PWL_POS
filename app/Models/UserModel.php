7dc85eef7299080c28de0313b703984657c57306
Jihan Karunia Putri <jihankarunia09@gmail.com>
2024-05-08 15:29:22 +0700

    Jobsheet 10: Praktikum 1 - Membuat RESTful API Register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,14 +2,26 @@
 
 namespace App\Models;
 
+use App\Http\Middleware\Authenticate;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\LevelModel;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends \Illuminate\Foundation\Auth\User
+class UserModel extends Authenticatable implements JWTSubject
 {
+
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return[];
+    }
+    
     use HasFactory;
 
     protected $table = 'm_user';
